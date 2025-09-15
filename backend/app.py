281ff15e1b9ad6cb648bfae9aaa0f2cb3c8e2b4e
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2025-09-16 01:06:32 +0530

    fix issue

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-import os
 
 from algorithms.bubble import bubble_sort
 from algorithms.insertion import insertion_sort
@@ -8,8 +7,7 @@ from algorithms.merge import merge_sort_generator as merge_sort
 from algorithms.quick import quick_sort_generator as quick_sort
 from algorithms.heap import heap_sort
 
-# Serve the static files from the React build folder
-app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
+app = Flask(__name__)
 CORS(app)
 
 ALGORITHMS = {
@@ -36,15 +34,6 @@ def sort():
     steps = list(sort_generator)
     
     return jsonify(steps)
-
-# Add a catch-all route to serve the React index.html file
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run(use_reloader=True, port=5000, threaded=True)
